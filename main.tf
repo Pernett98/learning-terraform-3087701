4dfee10af3633f57cd4c124e56c5584aff0d2bf0
Sebastian Pernett <sebastian_pernett@epam.com>
2025-03-20 20:16:09 -0500

    code review

--- main.tf
+++ main.tf
@@ -30,16 +30,15 @@ resource "aws_instance" "blog" {
 }
 
 module "blog_sg" {
-  source = "terraform-aws-modules/security-group/aws"
+  source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  name = "blow"
+  vpc_id  = data.aws_vpc.default.id
+  name    = "blog"
 
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress_rules = ["http-80-tcp", "https-80-tcp"]
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
+  
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
